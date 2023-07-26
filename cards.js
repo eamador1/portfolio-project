3078057154973ac44c251066c230a2c5e81f8ebc
erwin amador <eamador0305@gmail.com>
2023-07-26 15:55:14 -0600

    Add image to pop-up

--- cards.js
+++ cards.js
@@ -7,8 +7,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'bootstrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/img/SnapshootPortfolio.svg',
+  imageMobile: 'img/img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -22,8 +22,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
  {
   id: '2',
@@ -33,8 +33,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'bootstrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/SnapshootPortfolio.svg',
+  imageMobile: 'img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -48,8 +48,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
  {
   id: '3',
@@ -59,8 +59,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'boostrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/SnapshootPortfolio.svg',
+  imageMobile: 'img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -74,8 +74,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
  {
   id: '4',
@@ -85,8 +85,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'bootstrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/SnapshootPortfolio.svg',
+  imageMobile: 'img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -100,8 +100,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
  {
   id: '5',
@@ -111,8 +111,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'bootstrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/SnapshootPortfolio.svg',
+  imageMobile: 'img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -126,8 +126,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
  {
   id: '6',
@@ -137,8 +137,8 @@ const projects = [
   paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
   languages: ['html', 'bootstrap', 'Ruby'],
   seeBtn: 'See Project',
-  imageDesktop: 'img/SnapshootPortfolioDesktop.png',
-  imageMobile: 'img/SnapshootPortfolioMobile.png',
+  imageDesktop: 'img/SnapshootPortfolio.svg',
+  imageMobile: 'img/SnapshootPortfolio.svg',
   desktopTitle: 'Keeping track of hundreds of components website 1',
   desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
    'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
@@ -152,8 +152,8 @@ const projects = [
   closeImage: 'img/x-mobile.png',
   liveBtn: 'See live',
   sourceBtn: 'See source',
-  live: 'https://eamador1.github.io/portfolio/',
-  source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
+  live: 'https://eamador1.github.io/portfolio-project/',
+  source: 'https://github.com/eamador1/portfolio-project',
  },
 ]
 
@@ -209,3 +209,206 @@ projects.forEach((project) => {
   $containerSeeprojectButton.appendChild($button);
   $containerCards.appendChild($card);
 });
+
+// DISPLAY MODAL WINDOW //
+
+function showModal(modalId) {
+    const popup = projects.find((popup) => popup.id === modalId);
+  
+    // Create Elements
+    const $modalContainer = document.createElement('div');
+  
+    const $modal = document.createElement('div');
+    const $closeBtn = document.createElement('button');
+    const $modalTitle = document.createElement('div');
+    // let $closeModal = document.createElement('img');
+    const $ulModal = document.createElement('ul');
+    const $pModalMobile = document.createElement('p');
+    const $live = document.createElement('button');
+    const $aLive = document.createElement('a');
+    const $source = document.createElement('button');
+    const $aSource = document.createElement('a');
+    const $divParagraph = document.createElement('div');
+  
+    // Giving Classes
+    $modalContainer.classList.add('modalContainer');
+  
+    $modal.classList.add('modal');
+    $closeBtn.classList.add('x-modalClose');
+    $modalTitle.classList.add('modal-title');
+    // $closeModal.classList.add('x-modalClose');
+    $ulModal.classList.add('modal-tools');
+    $pModalMobile.classList.add('p-modalMobile');
+    $live.classList.add('live', 'button');
+    $source.classList.add('source', 'button');
+    $divParagraph.classList.add('divMP');
+  
+    // Assigning Value
+    $modalTitle.textContent = popup.mobileTitle;
+    $closeBtn.innerHTML = '&times';
+    $pModalMobile.textContent = popup.mobileText;
+    $live.textContent = popup.liveBtn;
+    $source.textContent = popup.sourceBtn;
+    $aLive.href = popup.live;
+    $aLive.target = '_blank';
+    $aLive.rel = 'noopener';
+    $aSource.href = popup.source;
+    $aSource.target = '_blank';
+    $aSource.rel = 'noopener';
+  
+    popup.tools.forEach((tool) => {
+      const $liModal = document.createElement('li');
+      $liModal.classList.add('modal-list');
+      $liModal.textContent = tool;
+      $ulModal.appendChild($liModal);
+    });
+  
+    $modal.appendChild($modalTitle);
+    $modal.appendChild($closeBtn);
+    $modal.appendChild($ulModal);
+    $divParagraph.appendChild($pModalMobile);
+    $modal.appendChild($divParagraph);
+    $aLive.appendChild($live);
+    $modal.appendChild($aLive);
+    $aSource.appendChild($source);
+    $modal.appendChild($aSource);
+  
+    $modalContainer.appendChild($modal);
+  
+    document.body.appendChild($modalContainer);
+  
+    // to open tha modal
+    const modalContainer = document.querySelector('.modalContainer');
+    modalContainer.style.display = 'block';
+  
+    // to close modal
+    const closeModal = document.querySelector('.x-modalClose');
+    closeModal.addEventListener('click', () => {
+      modalContainer.style.display = 'none';
+    });
+  }
+  
+  const openModal = document.querySelectorAll('.button');
+  
+  const openFirstBtn = document.querySelectorAll('.button1');
+  
+  const popup1 = {
+    id: '1',
+    images: './img/img-placeholder1.png',
+    image1: 'img/image-hover.png',
+    title: 'Professional Art Printing',
+    paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    languages: ['html', 'bootstrap', 'Ruby'],
+    seeBtn: 'See Project',
+    imageDesktop: 'img/SnapshootPortfolio.svg',
+    imageMobile: 'img/SnapshootPortfolio.svg',
+    desktopTitle: 'Keeping track of hundreds of components website 1',
+    desktopText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s.<br>',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it in 1960s with the releax  map lapora verita.'],
+    mobileTitle: 'Multi-Post Stories',
+    mobileText: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+      'electronic typesetting, remaining essent'],
+    tools: ['html', 'bootstrap', 'Ruby on Rails'],
+    closeImage: 'img/x-mobile.png',
+    liveBtn: 'See live',
+    sourceBtn: 'See source',
+    live: 'https://eamador1.github.io/portfolio-project/',
+    source: 'https://github.com/eamador1/portfolio-project',
+  };
+  
+  function showModal1() {
+    const popup = popup1;
+  
+    // Create Elements
+    const $modalContainer = document.createElement('div');
+  
+    const $modal = document.createElement('div');
+    const $closeBtn = document.createElement('button');
+    const $modalTitle = document.createElement('div');
+    // let $closeModal = document.createElement('img');
+    const $ulModal = document.createElement('ul');
+    const $pModalMobile = document.createElement('p');
+    const $live = document.createElement('button');
+    const $aLive = document.createElement('a');
+    const $source = document.createElement('button');
+    const $aSource = document.createElement('a');
+    const $divParagraph = document.createElement('div');
+  
+    // Giving Classes
+    $modalContainer.classList.add('modalContainer');
+  
+    $modal.classList.add('modal');
+    $closeBtn.classList.add('x-modalClose');
+    $modalTitle.classList.add('modal-title');
+    // $closeModal.classList.add('x-modalClose');
+    $ulModal.classList.add('modal-tools');
+    $pModalMobile.classList.add('p-modalMobile');
+    $live.classList.add('live', 'button');
+    $source.classList.add('source', 'button');
+    $divParagraph.classList.add('divMP');
+  
+    // Assigning Value
+    $modalTitle.textContent = popup.mobileTitle;
+    $closeBtn.innerHTML = '&times';
+    $pModalMobile.textContent = popup.mobileText;
+    $live.textContent = popup.liveBtn;
+    $source.textContent = popup.sourceBtn;
+    $aLive.href = popup.live;
+    $aLive.target = '_blank';
+    $aLive.rel = 'noopener';
+    $aSource.href = popup.source;
+    $aSource.target = '_blank';
+    $aSource.rel = 'noopener';
+  
+    popup.tools.forEach((tool) => {
+      const $liModal = document.createElement('li');
+      $liModal.classList.add('modal-list');
+      $liModal.textContent = tool;
+      $ulModal.appendChild($liModal);
+    });
+  
+    $modal.appendChild($modalTitle);
+    $modal.appendChild($closeBtn);
+    $modal.appendChild($ulModal);
+    $divParagraph.appendChild($pModalMobile);
+    $modal.appendChild($divParagraph);
+    $aLive.appendChild($live);
+    $modal.appendChild($aLive);
+    $aSource.appendChild($source);
+    $modal.appendChild($aSource);
+  
+    $modalContainer.appendChild($modal);
+  
+    document.body.appendChild($modalContainer);
+  
+    // to open tha modal
+    const modalContainer = document.querySelector('.modalContainer');
+    modalContainer.style.display = 'block';
+  
+    // to close modal
+    const closeModal = document.querySelector('.x-modalClose');
+    closeModal.addEventListener('click', () => {
+      modalContainer.style.display = 'none';
+    });
+  }
+  
+  openFirstBtn.forEach((button) => {
+    button.addEventListener('click', () => {
+      // get ID of popups objects
+      // const modalId = button.dataset.projectId;
+      // call the popup function with id
+      showModal1();
+    });
+  });
+  
+  openModal.forEach((button) => {
+    button.addEventListener('click', () => {
+      // get ID of popups objects
+      const modalId = button.dataset.projectId;
+      // call the popup function with id
+      showModal(modalId);
+    });
+  });
