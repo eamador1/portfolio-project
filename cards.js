aabc168d1d6e38bec171861b0ba6e01325b850a3
erwin amador <eamador0305@gmail.com>
2023-07-26 13:12:52 -0600

    Generate cards dynamicaly

--- cards.js
+++ cards.js
@@ -1,4 +1,5 @@
 const projects = [
+  {
   id: '1',
   images: './img/img-placeholder1.png',
   image1: 'img/image-hover.png',
@@ -155,3 +156,56 @@ const projects = [
   source: 'https://github.com/eamador1/portfolio/blob/main/index.html',
  },
 ]
+
+const $containerCards = document.querySelector('.container-cards');
+
+projects.forEach((project) => {
+  const $card = document.createElement('div');
+  const $hoverImage = document.createElement('img');
+  const $containerCardHeading = document.createElement('div');
+  const $cardHeading = document.createElement('h4');
+  const $containerCardParagraph = document.createElement('div');
+  const $cardParagraph = document.createElement('p');
+  const $cardList = document.createElement('ul');
+  const $containerSeeprojectButton = document.createElement('div');
+  const $button = document.createElement('button');
+
+  //Assign Classes
+
+  $card.classList.add('card');
+  $hoverImage.classList.add('image-hover');
+  $containerCardHeading.classList.add('container-card-heading');
+  $cardHeading.classList.add('card-heading');
+  $containerCardParagraph.classList.add('container-card-paragraph');
+  $cardParagraph.classList.add('card-paragraph');
+  $cardList.classList.add('cardlist');
+  $containerSeeprojectButton.classList.add('container-seeproject-button');
+  $button.classList.add('button');
+
+  //Assign Values
+
+  $hoverImage.setAttribute('src', project.image1);
+  $cardHeading.textContent = project.title;
+  $cardParagraph.textContent = project.paragraph;
+  $button.textContent = project.seeBtn;
+  $button.dataset.projectId = project.id;
+
+  project.languages.forEach((language) => {
+    const $li = document.createElement('li');
+    $li.classList.add('list');
+    $li.textContent = language;
+    $cardList.appendChild($li);
+  });
+
+  // Append elements
+
+  $card.appendChild($hoverImage);
+  $card.appendChild($containerCardHeading);
+  $card.appendChild($containerCardParagraph);
+  $card.appendChild($cardList);
+  $card.appendChild($containerSeeprojectButton);
+  $containerCardHeading.appendChild($cardHeading);
+  $containerCardParagraph.appendChild($cardParagraph);
+  $containerSeeprojectButton.appendChild($button);
+  $containerCards.appendChild($card);
+});
